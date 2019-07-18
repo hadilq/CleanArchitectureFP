e8a47559dcede120df247efa3f06b1eda9a3a2c1
Hadi <hadilashkari@gmail.com>
2019-07-18 21:41:23 +0430

    Verify all checks

--- app/src/androidTest/java/com/github/hadilq/cleanarchitecturefp/util/ViewActionUtil.kt
+++ app/src/androidTest/java/com/github/hadilq/cleanarchitecturefp/util/ViewActionUtil.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.test.espresso.PerformException
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.util.HumanReadables
@@ -15,6 +16,7 @@ import java.util.concurrent.TimeoutException
 
 
 object ViewActionUtil {
+
 
     fun waitId(viewId: Int, millis: Long): ViewAction {
         return object : ViewAction {
@@ -37,6 +39,45 @@ object ViewActionUtil {
                     for (child in TreeIterables.breadthFirstViewTraversal(view)) {
                         // found view with required ID
                         if (viewMatcher.matches(child)) {
+                            return
+                        }
+                    }
+
+                    uiController?.loopMainThreadForAtLeast(50)
+                } while (System.currentTimeMillis() < endTime)
+
+                // timeout happens
+                throw PerformException.Builder()
+                    .withActionDescription(this.description)
+                    .withViewDescription(HumanReadables.describe(view))
+                    .withCause(TimeoutException())
+                    .build()
+            }
+        }
+    }
+
+    fun waitIndex(matcher: Matcher<View>, index: Int, millis: Long, viewAssert: ViewAssertion): ViewAction {
+        return object : ViewAction {
+
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
+
+            override fun getDescription(): String {
+                return "wait for a specific view during $millis millis."
+            }
+
+            override fun perform(uiController: UiController?, view: View?) {
+                uiController?.loopMainThreadUntilIdle()
+                val startTime = System.currentTimeMillis()
+                val endTime = startTime + millis
+                val viewMatcher = withIndex(matcher, index)
+
+                do {
+                    for (child in TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required matcher
+                        if (viewMatcher.matches(child)) {
+                            viewAssert.check(child, null)
                             return
                         }
                     }
